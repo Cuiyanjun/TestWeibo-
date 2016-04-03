4e4950e086b0c0941c419c082fad7facd8cc325e
cyj <1559384362@qq.com>
2016-04-03 17:36:24 +0800

    初始化

---
+++ weibo/weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo
+//
+//  Created by hxshop on 16/4/3.
+//  Copyright © 2016年 hxshop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
